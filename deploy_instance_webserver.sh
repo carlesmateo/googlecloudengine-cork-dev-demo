bba0cc9ac8a1ce1bc8573c117bb4726877e21f91
Carles Mateo <carles.mateo@mpstor.com>
2019-02-06 11:52:10 +0000

    Updated single instance script

--- deploy_instance_webserver.sh
+++ deploy_instance_webserver.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install -y git
-apt-get install -y apache2
+apt update
+apt upgrade
+apt install -y git
+apt install -y apache2
 cat <<EOF > /var/www/html/index.html
 <html><body><h1>Hello World</h1>
 <p>This page was created from a simple startup script!</p>
