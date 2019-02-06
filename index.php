ba51563909bf40fe9b7a44094ffe8e3193ffbce9
Carles Mateo <carles.mateo@mpstor.com>
2019-02-06 11:22:11 +0000

    index.php and image

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Google Developer Group Cork</title>
+</head>
+<body>
+    <div align="center">
+        <img src="img/highres_478304377.jpeg.png" />
+    </div>
+</body>
+</html
