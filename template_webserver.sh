9dd5173e5d2e9b0a5dbc13235a8d81dc0fcc3222
Carles Mateo <carles.mateo@mpstor.com>
2019-02-06 11:46:57 +0000

    Singe instance script

--- template_webserver.sh
+++ template_webserver.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 # Note: sudo is not necessary as the Startup script is always run as root
+PATH_GIT="/root/"
+PATH_WEB="/var/www/html/"
 apt update
-apt -y upgrade
+apt upgrade
 # We always git
-apt -y install git
+apt install -y git
 # Download our Demo stuff
-git clone https://github.com/carlesmateo/googlecloudengine-cork-dev-demo.git
+git clone https://github.com/carlesmateo/googlecloudengine-cork-dev-demo.git ${PATH_GIT}
+# Copy to right place
+cp ${PATH_GIT}index.php ${PATH_WEB}
+mkdir ${PATH_WEB}img
+cp ${PATH_GIT}*.png ${PATH_WEB}
 # Nice tools to have
 apt -y install htop ncdu mc lynx fatrace
 # LAMP Installation
