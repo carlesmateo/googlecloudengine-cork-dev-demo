8fec222a1457965ba6fe5d42bd00f8b2968945dc
Carles Mateo <carles.mateo@mpstor.com>
2019-02-06 12:49:58 +0000

    Added iperf

--- template_webserver.sh
+++ template_webserver.sh
@@ -6,15 +6,17 @@ apt update
 apt -y upgrade
 # We always git
 apt install -y git
+# Nice tools to have
+apt -y install htop ncdu mc lynx fatrace iperf
+# LAMP Installation
+apt -y install apache2
+apt -y install mysql-server
+apt -y install php libapache2-mod-php
 # Download our Demo stuff
 git clone https://github.com/carlesmateo/googlecloudengine-cork-dev-demo.git ${PATH_GIT}
 # Copy to right place
 cp ${PATH_GIT}index.php ${PATH_WEB}
 mkdir ${PATH_WEB}img
 cp ${PATH_GIT}*.png ${PATH_WEB}
-# Nice tools to have
-apt -y install htop ncdu mc lynx fatrace
-# LAMP Installation
-apt -y install apache2
-apt -y install mysql-server
-apt -y install php libapache2-mod-php
+# Delete the original Apache file
+rm ${PATH_WEB}index.html
